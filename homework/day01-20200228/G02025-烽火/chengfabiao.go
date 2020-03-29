e08239839c9b20ec2f470ae7c2527115c88fc8b4
devon6686 <512795069@qq.com>
2020-03-29 20:49:08 +0800

    update chengfabiao

--- homework/day01-20200228/G02025-烽火/chengfabiao.go
+++ homework/day01-20200228/G02025-烽火/chengfabiao.go
@@ -5,13 +5,9 @@ import "fmt"
 func main() {
 
 	for i := 1; i < 10; i++ {
-		for j := 1; j < 10; j++ {
-			if i >= j {
-				fmt.Printf("%d * %d = %2d\t", i, j, i*j)
-			}
-			if i == j {
-				fmt.Println()
-			}
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d * %d = %2d\t", i, j, i*j)
 		}
+		fmt.Println()
 	}
 }
