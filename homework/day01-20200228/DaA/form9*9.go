ec69b2ba2cc1ac68d6d9b793f8120bed152c0114
xxdu521 <xxdu521@163.com>
2020-03-29 21:43:15 +0800

    homework

---
+++ homework/day01-20200228/DaA/form9*9.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	9*9乘法表
+	1*1=1
+	2*1=1 2*2=4
+	3*1=3 3*2=6 3*3=9
+
+	1.循环主数
+	2.循环辅数，辅助小于等于主数
+	*/
+
+	for i := 1; i < 10 ; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%-2d ", i, j, i*j)
+		}
+		fmt.Println()
+	}
+
+}
