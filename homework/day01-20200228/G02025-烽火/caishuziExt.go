ae74a493b1545dd788ed11da01477218348270d6
devon6686 <512795069@qq.com>
2020-03-31 22:20:21 +0800

    modify 猜数字

---
+++ homework/day01-20200228/G02025-烽火/caishuziExt.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var count = 1
+	var randNum int
+	rand.Seed(time.Now().Unix())
+	randNum = rand.Intn(100)
+	fmt.Println(randNum)
+	var userNum int
+	var choice string
+	var flag bool = true
+
+	for flag {
+		fmt.Println("请输入您猜测的数字：")
+		fmt.Scan(&userNum)
+		if count < 5 {
+			if userNum == randNum {
+				fmt.Printf("经过%d次终于猜对了，太聪明了!\n", count)
+				break
+			} else if userNum > randNum {
+				count++
+				fmt.Println("猜的太大了")
+			} else {
+				count++
+				fmt.Println("猜的太小了")
+			}
+		} else {
+			fmt.Println("5次用完还没猜对，游戏结束! 是否还要继续?[y/n]")
+			fmt.Scan(&choice)
+			switch choice {
+			case "Y", "y", "yes":
+				randNum = rand.Intn(100)
+				fmt.Println(randNum)
+				count = 1
+				// fmt.Println("new", count)
+			default:
+				flag = false
+			}
+
+		}
+	}
+
+}
