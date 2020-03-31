4ea5feb76a2a3080d6b78ba86554f4e8cb677778
tony3154 <tyrone3154@gmail.com>
2020-03-31 16:54:06 +0800

    代码格式化

--- homework/day01-20200228/G02002-刘勇/multiplication.go
+++ homework/day01-20200228/G02002-刘勇/multiplication.go
@@ -1,9 +1,11 @@
 package main
+
 import "fmt"
+
 func main() {
-	for i :=1;i<=9;i++ {
-		for j:=1;j<=i;j++{
-			fmt.Printf("%d*%d=%-5d",j,i,i*j)
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%-5d", j, i, i*j)
 		}
 		fmt.Println()
 	}
