4f2119d2878087a218d008f6c4ae70de641d3416
jibaoman <jibaoman@163.com>
2020-04-01 08:31:41 +0800

    添加注释

--- homework/day01-20200228/GO2036/rondom_numble/rm_num.go
+++ homework/day01-20200228/GO2036/rondom_numble/rm_num.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
+	//
 	rand.Seed(time.Now().UnixNano())
+
+	//生成100以内的随机整数
 	a := rand.Intn(100)
 	fmt.Println(a)
 
@@ -25,6 +28,8 @@ func main() {
 			fmt.Println("恭喜你，答对了！")
 			break
 		}
+
+		//尝试五次后,如果用户继续玩游戏，则重新循环
 		if i == 5 {
 			var ch string
 			fmt.Print("五次机会已经使用完,是否继续游戏Y|N:")
