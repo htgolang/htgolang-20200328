85fd617c1778ed1f730063f79772ca5e4beff1c0
root <904101810@qq.com>
2020-04-09 17:25:34 +0800

    改进
    Signed-off-by: root <904101810@qq.com>

--- homework/day01-20200228/GO2034-汉特/Multiplication.go
+++ homework/day01-20200228/GO2034-汉特/Multiplication.go
@@ -4,23 +4,14 @@ import "fmt"
 
 func main() {
 	for aVal := 1; aVal <= 9; aVal++ {
-		for bVal := 1; bVal <= 9; bVal++ {
-			if aVal < bVal {
-				continue
-			}
-			// fmt.Printf("%d%v%d%v%02d\t",bVal,"x",aVal,"=",(bVal*aVal))
+		for bVal := 1; bVal <= aVal; bVal++ {
 			fmt.Printf("%dx%d=%02d\t", bVal, aVal, (bVal * aVal))
-			// fmt.Print(bVal,"x",aVal,"=")
-			// fmt.Printf("%02d  ",aVal*bVal)
 		}
 		fmt.Println("")
 	}
-	for aVal := 1; aVal <= 9; aVal++ {
-		for bVal := 1; bVal <= 9; bVal++ {
-			if aVal > bVal {
-				continue
-			}
-			fmt.Print(aVal, "x", bVal, "=")
+	for aVal := 9; aVal >= 1; aVal-- {
+		for bVal := 1; bVal <= aVal; bVal++ {
+			fmt.Print(bVal, "x", aVal, "=")
 			fmt.Printf("%02d  ", aVal*bVal)
 		}
 		fmt.Println("")
