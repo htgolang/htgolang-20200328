505f9ec512ac6aeed1ae251ac8ba047190f45ddb
devon6686 <512795069@qq.com>
2020-05-08 10:31:11 +0800

    todolistv2作业

---
+++ homework/day04-20200425/Go2025-烽火/todolistv2/go.mod
@@ -0,0 +1,9 @@
+module todolistv2
+
+go 1.13
+
+require (
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+)
---
+++ homework/day04-20200425/Go2025-烽火/todolistv2/todolistv2.go
@@ -0,0 +1,297 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/howeyc/gopass"
+	"github.com/olekukonko/tablewriter"
+)
+
+// 命令行的任务管理器
+
+// 1. 任务的输入(添加任务)
+// 2. 任务列表(任务查询)
+// 3. 任务修改
+// 4. 任务删除
+// 5. 详情
+
+// 任务
+// ID, 任务名称， 开始时间， 结束时间，状态， 负责人
+// ID, name.,start_time, end_time, status, user
+// []map[string][string]
+
+const (
+	name      = "name"
+	startTime = "startTime"
+	endTime   = "endTime"
+	status    = "status"
+	user      = "user"
+	password  = "b0230d6ac1b3b3bcff28ace36d15ad5d" // hello
+	salt      = "02P8bA"
+)
+
+const (
+	statusNew      = "未执行"
+	statusComplete = "完成"
+	statusBegin    = "开始执行"
+	statusPause    = "暂停"
+)
+
+var statusChoice []string = []string{statusNew, statusBegin, statusComplete, statusPause}
+
+// var todos = make([]map[string]string, 0)
+var todos = []map[string]string{
+	{"id": "1", "name": "study", "startTime": "17:00", "endTime": "19:00", "status": statusNew, "user": "devon"},
+	{"id": "2", "name": "sleep", "startTime": "19:00", "endTime": "20:00", "status": statusNew, "user": "devon"},
+	{"id": "3", "name": "eat", "startTime": "18:00", "endTime": "19:00", "status": statusNew, "user": "devon"},
+}
+
+func input(prompt string) string {
+	var text string
+	fmt.Print(prompt)
+	fmt.Scan(&text)
+	return strings.TrimSpace(text)
+}
+
+func genId() int {
+	// 生成最大的id
+	var rt int
+	for _, todo := range todos {
+		todoId, _ := strconv.Atoi(todo["id"])
+		if rt < todoId {
+			rt = todoId
+		}
+	}
+	return rt + 1
+}
+
+func newTask() map[string]string {
+	task := make(map[string]string)
+	task["id"] = strconv.Itoa(genId())
+	task[name] = ""
+	task[startTime] = ""
+	task[endTime] = ""
+	task[status] = statusNew
+	task[user] = ""
+	return task
+}
+
+func renderTask(tasks []map[string]string) {
+	header := []string{"ID", "name", startTime, endTime, status, user}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAlignment(tablewriter.ALIGN_CENTER)
+	table.SetHeader(header)
+	for i := 0; i < len(header); i++ {
+		table.SetColMinWidth(i, 20)
+	}
+	for _, task := range tasks {
+		table.Append([]string{task["id"], task[name], task[startTime], task[endTime], task[status], task[user]})
+	}
+	table.Render()
+	// table.AppendBulk(content)
+}
+
+func add() {
+	task := newTask()
+	fmt.Println("请输入任务信息:")
+
+	for {
+		tempName := input("任务名:")
+		if verify_name(tempName) {
+			task[name] = tempName
+			break
+		} else {
+			fmt.Println("任务名称已存在!")
+		}
+	}
+	task[startTime] = input("开始时间:")
+	task[user] = input("负责人:")
+	todos = append(todos, task)
+	content := []map[string]string{task}
+	renderTask(content)
+}
+
+// func query() {
+// 	q := input("请输入查询信息:")
+// 	var content []map[string]string
+// 	for _, task := range todos {
+// 		if q == "all" || strings.Contains(task[name], q) {
+// 			content = append(content, task)
+// 		}
+// 	}
+// 	if len(content) == 0 {
+// 		fmt.Println("未找到关联任务!")
+// 	} else {
+// 		renderTask(content)
+// 	}
+// }
+
+func sortTask(tasks []map[string]string, key string) []map[string]string {
+	if key == "name" || key == "startTime" {
+		sort.Slice(tasks, func(i, j int) bool { return tasks[i][key] < tasks[j][key] })
+	}
+	return tasks
+}
+
+func querySort() {
+	q := input("请输入查询信息:")
+	content := make([]map[string]string, 0)
+	for _, task := range todos {
+		if q == "all" || strings.Contains(task[name], q) {
+			content = append(content, task)
+		}
+	}
+	if len(content) == 0 {
+		fmt.Println("未找到关联任务!")
+	} else {
+		key := input("请输入排序方式(name/startTime):")
+		newTasks := sortTask(content, key)
+		renderTask(newTasks)
+	}
+}
+
+func modify() {
+	q := input("请输入需要修改的任务ID:")
+	for _, task := range todos {
+		if q == task["id"] {
+			content := []map[string]string{task}
+			renderTask(content)
+			switch input("是否确认修改(y/yes):") {
+			case "y", "yes":
+				for {
+					tempName := input("任务名称:")
+					if verify_name(tempName) {
+						task[name] = tempName
+						break
+					} else {
+						fmt.Println("任务名称已存在!")
+					}
+				}
+				task[startTime] = input("开始时间:")
+
+				for {
+					tempStaus := input("状态:")
+					if verify_status(tempStaus) {
+						if tempStaus == statusComplete {
+							task[endTime] = time.Now().Format("2006-01-02 15:04:05")
+						}
+						task[status] = tempStaus
+						break
+					} else {
+						fmt.Println("输入的状态值不对!可选范围:", strings.Join(statusChoice, ", "))
+
+					}
+				}
+				fmt.Println("任务修改完成")
+				renderTask(content)
+			default:
+				fmt.Println("取消修改")
+				break
+			}
+		}
+	}
+}
+
+func remove() {
+	q := input("请输入需要删除的任务ID:")
+	for index, task := range todos {
+		if q == task["id"] {
+			content := []map[string]string{task}
+			renderTask(content)
+			switch input("是否进行删除(y/yes):") {
+			case "y", "yes":
+				copy(todos[index:], todos[index+1:])
+				newTasks := todos[:len(todos)-1]
+				fmt.Printf("任务ID:%s 已删除"， q)
+			default:
+				fmt.Println("取消删除")
+			}
+		}
+	}
+}
+
+func verify_name(inputName string) bool {
+	for _, task := range todos {
+		if inputName == task[name] {
+			return false
+		}
+	}
+	return true
+}
+
+func verify_status(inputStatus string) bool {
+	for _, status := range statusChoice {
+		if inputStatus == status {
+			return true
+		}
+	}
+	return false
+}
+
+func saltMd5(input string) string {
+	hasher := md5.New()
+	io.WriteString(hasher, input)
+	io.WriteString(hasher, salt)
+
+	cryptoPasswd := fmt.Sprintf("%x", hasher.Sum(nil))
+	return cryptoPasswd
+}
+
+func getPassword() string {
+	fmt.Print("请输入密码: ")
+	passwd, _ := gopass.GetPasswdMasked()
+	return string(passwd)
+}
+
+func main() {
+	var (
+		count = 0
+		limit = 3
+	)
+
+	methods := map[string]func(){
+		"add":    add,
+		"query":  querySort,
+		"modify": modify,
+		"delete": remove,
+	}
+
+	for {
+		if count < limit {
+			input := getPassword()
+			count++
+			if saltMd5(input) == password {
+				fmt.Println("密码验证成功，请进行后续操作!")
+				break
+			} else {
+				if count != 3 {
+					fmt.Printf("密码错误，还有%d次机会\n", limit-count)
+				}
+			}
+		} else {
+			fmt.Println("3次密码验证错误，程序退出")
+			os.Exit(1)
+		}
+	}
+
+	for {
+		text := input("请输入操作[add/query/modify/delete/exit]:")
+		if text == "exit" {
+			break
+		}
+
+		if method, ok := methods[text]; ok {
+			method()
+		} else {
+			fmt.Println("输入的指令错误")
+		}
+	}
+
+}
